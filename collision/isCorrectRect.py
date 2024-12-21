7361e951eca903dcb6f7839028dfc909b6a55fe7
Lizok636 <egorovaelizaveta041@gmail.com>
2024-12-21 22:09:29 +0300

    Update isCorrectRect.py

--- collision/isCorrectRect.py
+++ collision/isCorrectRect.py
@@ -1,5 +1,23 @@
-def isCorrectRect(rect):
-    if rect[0][0] < rect[1][0] and rect[0][1] < rect[1][1]:
-        return True
-    else:
+def isCorrectRect(coordinates):
+    LeftCoord,RightCoord=coordinates
+    xLeft, yLeft=LeftCoord
+    xRight,yRight=RightCoord
+    if not isinstance(coordinates,list) or len(coordinates)!=2:
         return False
+    elif not (
+        isinstance(xLeft, (int, float))
+        and isinstance(yLeft, (int, float))
+        and isinstance(xRight, (int, float))
+        and isinstance(yRight, (int, float))
+        ):
+        return False
+        
+    elif ((xLeft > xRight) or (yLeft > yRight)):
+        return False
+    elif not all(
+        isinstance(coord_pair, tuple) and len(coord_pair) == 2
+        for coord_pair in coordinates):
+
+        return False
+        
+    return True
